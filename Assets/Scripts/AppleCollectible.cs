3acd897253977e3fe2a2a74ca42ebfba03a6f3ac
Micah <micahtam70@gmail.com>
2026-04-12 22:42:48 -0400

    added apple collection sound

--- Assets/Scripts/AppleCollectible.cs
+++ Assets/Scripts/AppleCollectible.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 [RequireComponent(typeof(SphereCollider))]
 public class AppleCollectible : MonoBehaviour
 {
+    [SerializeField] private AudioClip dingSound;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Rabbit"))
         {
+            if (dingSound != null)
+                AudioSource.PlayClipAtPoint(dingSound, transform.position);
+
             GameManager.Instance.CollectApple();
             gameObject.SetActive(false);
         }
